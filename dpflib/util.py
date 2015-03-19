ff22f2c6675cc070f501e5b11f4f4770fca9809f
Ingmar Steen <iksteen@gmail.com>
2015-03-19 12:35:23 +0100

    Fix indention of deBruijn in dpflib.util.

--- dpflib/util.py
+++ dpflib/util.py
@@ -9,30 +9,30 @@ __all__ = [
 
 
 def deBruijn(n, k):
-   '''
-   An implementation of the FKM algorithm for generating the de Bruijn
-   sequence containing all k-ary strings of length n, as described in
-   "Combinatorial Generation" by Frank Ruskey.
-   '''
+    '''
+    An implementation of the FKM algorithm for generating the de Bruijn
+    sequence containing all k-ary strings of length n, as described in
+    "Combinatorial Generation" by Frank Ruskey.
+    '''
 
-   a = [ 0 ] * (n + 1)
+    a = [ 0 ] * (n + 1)
 
-   def gen(t, p):
-      if t > n:
-         for v in a[1:p + 1]:
-           yield v
-      else:
-         a[t] = a[t - p]
+    def gen(t, p):
+        if t > n:
+            for v in a[1:p + 1]:
+                yield v
+        else:
+            a[t] = a[t - p]
          
-         for v in gen(t + 1, p):
-           yield v
+            for v in gen(t + 1, p):
+                yield v
          
-         for j in xrange(a[t - p] + 1, k):
-            a[t] = j
-            for v in gen(t + 1, t):
-              yield v
-   
-   return gen(1, 1)
+            for j in xrange(a[t - p] + 1, k):
+                a[t] = j
+                for v in gen(t + 1, t):
+                    yield v
+
+    return gen(1, 1)
 
 
 def cycle(length, width=4, **kwargs):
