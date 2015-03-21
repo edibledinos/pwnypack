7459d05f5e40d360ac5c87ee44a3b7ff074573dd
Ingmar Steen <iksteen@gmail.com>
2015-03-21 13:43:05 +0100

    Cleanups and python3 support for asm.
    Format is now an enum, don’t depend on NamedTemporaryFile.unlink to
    exist.

--- pwnypack/asm.py
+++ pwnypack/asm.py
@@ -1,6 +1,8 @@
 import subprocess
 import pwnypack.target
 import tempfile
+import os
+from enum import Enum
 
 
 __all__ = [
@@ -8,34 +10,60 @@ __all__ = [
 ]
 
 
-def asm(code, format='bin', bits=None, target=None):
-    if bits is None:
-        if target is None:
-            target = pwnypack.target.target
-        bits = target.bits
+class Asm(object):
+    class Format(Enum):
+        bin = 'bin'
+        ith = 'ith'
+        srec = 'srec'
+        aout = 'aout'
+        aoutb = 'aoutb'
+        coff = 'coff'
+        elf32 = 'elf32'
+        elf64 = 'elf64'
+        elfx32 = 'elfx32'
+        as86 = 'as86'
+        obj = 'obj'
+        win32 = 'win32'
+        win64 = 'win64'
+        rdf = 'rdf'
+        ieee = 'ieee'
+        macho32 = 'macho32'
+        macho64 = 'macho64'
+        dbg = 'dbg'
 
-    tmp = tempfile.NamedTemporaryFile(delete=False)
-    tmp.write('bits %d\n' % bits)
-    tmp.write(code)
-    tmp.close()
+    @classmethod
+    def __call__(cls, code, fmt=Format.bin, bits=None, target=None):
+        if not isinstance(fmt, cls.Format):
+            fmt = cls.Format(fmt)
 
-    try:
-        p = subprocess.Popen(
-            [
-                'nasm',
-                '-o',
-                '/dev/stdout',
-                '-f',
-                format,
-                tmp.name,
-            ],
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        )
-        stdout, stderr = p.communicate(code)
-        if p.returncode:
-            raise SyntaxError(stderr)
-        return stdout
-    finally:
-        tmp.unlink(tmp.name)
+        if bits is None:
+            if target is None:
+                target = pwnypack.target.target
+            bits = target.bits
+
+        tmp = tempfile.NamedTemporaryFile(delete=False)
+        tmp.write(('bits %d\n%s' % (bits, code)).encode('utf-8'))
+        tmp.close()
+
+        try:
+            p = subprocess.Popen(
+                [
+                    'nasm',
+                    '-o',
+                    '/dev/stdout',
+                    '-f',
+                    fmt.value,
+                    tmp.name,
+                ],
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
+            stdout, stderr = p.communicate()
+            if p.returncode:
+                raise SyntaxError(stderr.decode('utf-8'))
+            return stdout
+        finally:
+            os.unlink(tmp.name)
+
+asm = Asm()
