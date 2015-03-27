07ec65ae4f87b7b5ec6cb3c578eab72ca870b6c0
Ingmar Steen <iksteen@gmail.com>
2015-03-27 12:02:20 +0100

    Remove asm output format, add addr to asm.
    It’s beyond the scope of pwnypack.

--- pwnypack/asm.py
+++ pwnypack/asm.py
@@ -8,7 +8,6 @@ import pwnypack.main
 import pwnypack.codec
 import tempfile
 import os
-from enum import Enum
 
 
 __all__ = [
@@ -17,61 +16,37 @@ __all__ = [
 ]
 
 
-class Asm(object):
-    class Format(Enum):
-        bin = 'bin'
-        ith = 'ith'
-        srec = 'srec'
-        aout = 'aout'
-        aoutb = 'aoutb'
-        coff = 'coff'
-        elf32 = 'elf32'
-        elf64 = 'elf64'
-        elfx32 = 'elfx32'
-        as86 = 'as86'
-        obj = 'obj'
-        win32 = 'win32'
-        win64 = 'win64'
-        rdf = 'rdf'
-        ieee = 'ieee'
-        macho32 = 'macho32'
-        macho64 = 'macho64'
-        dbg = 'dbg'
+def asm(code, addr=0, target=None):
+    if target is None:
+        target = pwnypack.target.target
 
-    def __call__(self, code, fmt=Format.bin, target=None):
-        if not isinstance(fmt, self.Format):
-            fmt = self.Format(fmt)
+    if target.arch is not pwnypack.target.Target.Arch.x86:
+        raise NotImplementedError('Only x86 is currently supported.')
 
-        if target is None:
-            target = pwnypack.target.target
-        assert target.arch is pwnypack.target.Target.Arch.x86, 'Only x86 is currently supported.'
+    tmp = tempfile.NamedTemporaryFile(delete=False)
+    tmp.write(('bits %d\norg %d\n%s' % (target.bits.value, addr, code)).encode('utf-8'))
+    tmp.close()
 
-        tmp = tempfile.NamedTemporaryFile(delete=False)
-        tmp.write(('bits %d\n%s' % (target.bits.value, code)).encode('utf-8'))
-        tmp.close()
-
-        try:
-            p = subprocess.Popen(
-                [
-                    'nasm',
-                    '-o',
-                    '/dev/stdout',
-                    '-f',
-                    fmt.value,
-                    tmp.name,
-                ],
-                stdin=subprocess.PIPE,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-            )
-            stdout, stderr = p.communicate()
-            if p.returncode:
-                raise SyntaxError(stderr.decode('utf-8'))
-            return stdout
-        finally:
-            os.unlink(tmp.name)
-
-asm = Asm()
+    try:
+        p = subprocess.Popen(
+            [
+                'nasm',
+                '-o',
+                '/dev/stdout',
+                '-f',
+                'bin',
+                tmp.name,
+            ],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        stdout, stderr = p.communicate()
+        if p.returncode:
+            raise SyntaxError(stderr.decode('utf-8'))
+        return stdout
+    finally:
+        os.unlink(tmp.name)
 
 
 def disasm(code, addr=0, target=None):
@@ -110,15 +85,14 @@ def asm_app(parser, cmd, args):  # pragma: no cover
     parser.add_argument('source', help='the code to assemble, read from stdin if omitted', nargs='?')
     pwnypack.main.add_target_arguments(parser)
     parser.add_argument(
-        '--output-format', '-F',
-        choices=asm.Format.__members__.keys(),
-        default=asm.Format.bin.name,
-        help='the output format',
+        '--address', '-o',
+        type=lambda v: int(v, 0),
+        default=0,
+        help='the address where the code is expected to run',
     )
 
     args = parser.parse_args(args)
     target = pwnypack.main.target_from_arguments(args)
-    fmt = asm.Format(asm.Format.__members__[args.output_format])
     if args.source is None:
         args.source = sys.stdin.read()
     else:
@@ -126,7 +100,6 @@ def asm_app(parser, cmd, args):  # pragma: no cover
 
     return asm(
         args.source,
-        fmt=fmt,
         target=target,
     )
 
--- tests/test_asm.py
+++ tests/test_asm.py
@@ -2,48 +2,58 @@ from nose.tools import raises
 import pwny
 
 
-SOURCE = 'mov al, [0xced]'
+SOURCE_ASM = 'mov al, [0xced]'
 RESULT_BIN_32 = b'\xa0\xed\x0c\x00\x00'
 RESULT_BIN_64 = b'\x8a\x04%\xed\x0c\x00\x00'
-RESULT_ITH_32 = b':05000000A0ED0C000062\n:00000001FF\n'
-RESULT_ITH_64 = b':070000008A0425ED0C00004D\n:00000001FF\n'
+
+
+SOURCE_DISASM = b'\x5f'
+RESULT_DISASM_32 = ['pop edi']
+RESULT_DISASM_64 = ['pop rdi']
 
 
 target_x86_32 = pwny.Target('x86', 32)
 target_x86_64 = pwny.Target('x86', 64)
+target_arm_32 = pwny.Target('arm', 32)
 
 
 def test_asm_with_default_target():
-    if pwny.target.arch != 'x86':
-        # Test only supported on x86.
-        return
-
-    if pwny.target.bits == 32:
-        assert pwny.asm(SOURCE) == RESULT_BIN_32
-    else:
-        assert pwny.asm(SOURCE) == RESULT_BIN_64
+    # Note: set up sets default arch to x86 32bit
+    assert pwny.asm(SOURCE_ASM) == RESULT_BIN_32
 
 
 def test_asm_with_target_x86_32():
-    assert pwny.asm(SOURCE, target=target_x86_32) == RESULT_BIN_32
+    assert pwny.asm(SOURCE_ASM, target=target_x86_32) == RESULT_BIN_32
+
 
 def test_asm_with_target_x86_64():
-    assert pwny.asm(SOURCE, target=target_x86_64) == RESULT_BIN_64
-
-
-def test_asm_with_format_x86_32():
-    assert pwny.asm(SOURCE, fmt=pwny.asm.Format.ith, target=target_x86_32) == RESULT_ITH_32
-
-
-def test_asm_with_format_x86_64():
-    assert pwny.asm(SOURCE, fmt=pwny.asm.Format.ith, target=target_x86_64) == RESULT_ITH_64
-
-
-@raises(ValueError)
-def test_asm_invalid_format():
-    pwny.asm(SOURCE, fmt='ced')
+    assert pwny.asm(SOURCE_ASM, target=target_x86_64) == RESULT_BIN_64
 
 
 @raises(SyntaxError)
 def test_asm_syntax_error():
     pwny.asm('mov ced, 3')
+
+
+@raises(NotImplementedError)
+def test_asm_unsupported_target():
+    pwny.asm(SOURCE_ASM, target=target_arm_32)
+
+
+def test_disasm_with_default_target():
+    # Note: set up sets default arch to x86 32bit
+    assert pwny.disasm(SOURCE_DISASM) == RESULT_DISASM_32
+
+
+def test_disasm_with_target_x86_32():
+    assert pwny.disasm(SOURCE_DISASM, target=target_x86_32) == RESULT_DISASM_32
+
+
+def test_disasm_with_target_x86_64():
+    print(RESULT_DISASM_64, pwny.disasm(SOURCE_DISASM, target=target_x86_64))
+    assert pwny.disasm(SOURCE_DISASM, target=target_x86_64) == RESULT_DISASM_64
+
+
+@raises(NotImplementedError)
+def test_disasm_unsupported_target():
+    pwny.disasm(SOURCE_DISASM, target=target_arm_32)
