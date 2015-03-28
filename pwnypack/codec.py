19c4d50cbb27aa729618f4e8ee113b49a8cb4105
Ingmar Steen <iksteen@gmail.com>
2015-03-28 19:32:47 +0100

    Add docstrings for codec module.

--- pwnypack/codec.py
+++ pwnypack/codec.py
@@ -1,3 +1,9 @@
+"""
+This module contains functions that allow you to manipulate, encode or decode
+strings and byte sequences.
+"""
+
+
 import base64
 import re
 import string
@@ -25,6 +31,32 @@ __all__ = [
 
 
 def xor(key, data):
+    """
+    Perform cyclical exclusive or operations on ``data``.
+
+    The ``key`` can be a an integer *(0 <= key < 256)* or a byte sequence. If
+    the key is smaller than the provided ``data``, the ``key`` will be
+    repeated.
+
+    Args:
+        key(int or bytes): The key to xor ``data`` with.
+        data(bytes): The data to perform the xor operation on.
+
+    Returns:
+        bytes: The result of the exclusive or operation.
+
+    Examples:
+        >>> from pwny import *
+        >>> xor(5, b'ABCD')
+        b'DGFA'
+        >>> xor(5, b'DGFA')
+        b'ABCD'
+        >>> xor(b'pwny', b'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+        b'15-=51)19=%5=9!)!%=-%!9!)-'
+        >>> xor(b'pwny', b'15-=51)19=%5=9!)!%=-%!9!)-')
+        b'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    """
+
     if type(key) is int:
         key = six.int2byte(key)
     key_len = len(key)
@@ -36,6 +68,35 @@ def xor(key, data):
 
 
 def caesar(shift, data, shift_ranges=('az', 'AZ')):
+    """
+    Apply a caesar cipher to a string.
+
+    The caesar cipher is a substition cipher where each letter in the given
+    alphabet is replaced by a letter some fixed number down the alphabet.
+
+    If ``shift`` is ``1``, *A* will become *B*, *B* will become *C*, etc...
+
+    You can define the alphabets that will be shift by specifying one or more
+    shift ranges. The characters will than be shifted within the given ranges.
+
+    Args:
+        shift(int): The shift to apply.
+        data(str): The string to apply the cipher to.
+        shift_ranges(list of str): Which alphabets to shift.
+
+    Returns:
+        str: The string with the caesar cipher applied.
+
+    Examples:
+        >>> caesar(16, 'Pwnypack')
+        'Fmdofqsa'
+        >>> caesar(-16, 'Fmdofqsa')
+        'Pwnypack'
+        >>> caesar(16, 'PWNYpack', shift_ranges=('AZ',))
+        'FMDOpack'
+        >>> caesar(16, 'PWNYpack', shift_ranges=('Az',))
+        '`g^iFqsA'
+    """
     alphabet = dict(
         (chr(c), chr((c - s + shift) % (e - s + 1) + s))
         for s, e in map(lambda r: (ord(r[0]), ord(r[-1])), shift_ranges)
@@ -46,9 +107,45 @@ def caesar(shift, data, shift_ranges=('az', 'AZ')):
 
 rot13_encode = codecs.getencoder('rot-13')
 rot13 = lambda d: rot13_encode(d)[0]
+rot13.__doc__ = """
+Rotate all characters in the alphabets A-Z and a-z by 13 positions in the
+alphabet. This is a :func:`caesar` shift of 13 along the fixed alphabets
+``A-Z`` and ``a-z``.
+
+Args:
+    d(str): The string to the apply the cipher to.
+
+Returns:
+    str: The string with the rot13 cipher applied.
+
+Examples:
+    >>> rot13('whax')
+    'junk'
+    >>> rot13('junk')
+    'whax'
+"""
 
 
 def enhex(d, separator=''):
+    """
+    Convert bytes to their hexadecimal representation, optionally joined by a
+    given separator.
+
+    Args:
+        d(bytes): The data to convert to hexadecimal representation.
+        separator(str): The separator to insert between hexadecimal tuples.
+
+    Returns:
+        str: The hexadecimal representation of ``d``.
+
+    Examples:
+        >>> from pwny import *
+        >>> enhex(b'pwnypack')
+        '70776e797061636b'
+        >>> enhex(b'pwnypack', separator=' ')
+        '70 77 6e 79 70 61 63 6b'
+    """
+
     v = binascii.hexlify(d).decode('ascii')
     if separator:
         return separator.join(
@@ -61,13 +158,73 @@ def enhex(d, separator=''):
 
 dehex_clean = re.compile('[^a-fA-F0-9]')
 dehex = lambda d: binascii.unhexlify(dehex_clean.sub('', d).encode('ascii'))
+dehex.__doc__ = """
+Convert a hexadecimal representation of a byte sequence to bytes. All
+non-hexadecimal characters will be removed from the input.
+
+Args:
+    d(str): The string of hexadecimal tuples.
+
+Returns:
+    bytes: The byte sequence represented by ``d``.
+
+Examples:
+    >>> from pwny import *
+    >>> dehex('70776e797061636b')
+    b'pwnypack'
+    >>> dhex('70 77 6e 79 70 61 63 6b')
+    b'pwnypack'
+"""
 
 
 enb64 = lambda d: base64.b64encode(d).decode('ascii')
+enb64.__doc__ = """
+Convert bytes to their base64 representation.
+
+Args:
+    d(bytes): The data to convert to its base64 representation.
+
+Returns:
+    str: The base64 representation of ``d``.
+
+Example:
+    >>> from pwny import *
+    >>> enb64(b'pwnypack')
+    'cHdueXBhY2s='
+"""
+
+
 deb64 = lambda d: base64.b64decode(d.encode('ascii'))
+deb64.__doc__ = """
+Convert a base64 representation back to its original bytes.
+
+Args:
+    d(str): The base64 representation to decode.
+
+Returns:
+    bytes: The bytes represented by ``d``.
+
+Example:
+    >>> from pwny import *
+    >>> deb64('cHdueXBhY2s=')
+    b'pwnypack'
+"""
 
 
-frequency = Counter
+frequency = lambda v: dict(Counter(v))
+frequency.__doc__ = """
+Perform a frequency analysis on a byte sequence or string.
+
+Args:
+    d(bytes or str): The sequence to analyse.
+
+Returns:
+    dict: A dictionary of unique elements in ``d`` and how often the occur.
+
+Example:
+    >>> frequency('pwnypack')
+    {'a': 1, 'c': 1, 'k': 1, 'n': 1, 'p': 2, 'w': 1, 'y': 1}
+"""
 
 
 @pwnypack.main.register('xor')
