f766b2f53018c5fc773ab981a75cf368b3e08cb3
Ingmar Steen <iksteen@gmail.com>
2016-04-05 13:34:50 +0200

    Add pwnbook jupyter notebook wrapper.

--- pwny/__init__.py
+++ pwny/__init__.py
@@ -6,6 +6,7 @@ from pwnypack.asm import *
 from pwnypack.elf import *
 from pwnypack.codec import *
 from pwnypack.shell import *
+from pwnypack.pwnbook import *
 from pwnypack.rop import *
 from pwnypack.fmtstring import *
 from pwnypack.php import *
