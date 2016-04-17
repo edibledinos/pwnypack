cf7367346f3ff96818c8198bd61468be9c3098db
Ingmar Steen <iksteen@gmail.com>
2016-04-18 00:27:57 +0200

    Use new bytecode module to translate opcodes.
    As an added bonus, we can now translate conditional statement from
    python 2.7/3.0 to python 2.6 (translate POP_JUMP_IF_FALSE into
    JUMP_IF_TRUE/POP_TOP).

--- pwnypack/pickle.py
+++ pwnypack/pickle.py
@@ -1,11 +1,12 @@
 import base64
-import opcode
 import types
 import warnings
 
 import six
 from six.moves import cPickle, copyreg
 from kwonly_args import kwonly_defaults
+
+import pwnypack.bytecode
 
 
 __all__ = ['pickle_invoke', 'pickle_func']
@@ -92,18 +93,9 @@ def pickle_invoke(func, target=None, protocol=None, *args):
     return cPickle.dumps(PickleInvoke(func, *args), protocol)
 
 
-# Opcode mappings for various python versions.
-OPMAPS = {
-    26: {'CALL_FUNCTION': 131, 'DUP_TOP': 4, 'INPLACE_FLOOR_DIVIDE': 28, 'BINARY_XOR': 65, 'END_FINALLY': 88, 'RETURN_VALUE': 83, 'POP_BLOCK': 87, 'SETUP_LOOP': 120, 'POP_TOP': 1, 'EXTENDED_ARG': 143, 'SETUP_FINALLY': 122, 'INPLACE_TRUE_DIVIDE': 29, 'CALL_FUNCTION_KW': 141, 'INPLACE_AND': 77, 'SETUP_EXCEPT': 121, 'STORE_NAME': 90, 'IMPORT_NAME': 107, 'LOAD_GLOBAL': 116, 'LOAD_NAME': 101, 'FOR_ITER': 93, 'EXEC_STMT': 85, 'DELETE_NAME': 91, 'BUILD_LIST': 103, 'COMPARE_OP': 106, 'BINARY_OR': 66, 'INPLACE_MULTIPLY': 57, 'STORE_FAST': 125, 'CALL_FUNCTION_VAR': 140, 'LOAD_LOCALS': 82, 'CONTINUE_LOOP': 119, 'PRINT_EXPR': 70, 'DELETE_GLOBAL': 98, 'GET_ITER': 68, 'STOP_CODE': 0, 'UNARY_NOT': 12, 'BINARY_LSHIFT': 62, 'JUMP_IF_TRUE': 112, 'LOAD_CLOSURE': 135, 'IMPORT_STAR': 84, 'INPLACE_OR': 79, 'BINARY_SUBTRACT': 24, 'STORE_MAP': 54, 'INPLACE_ADD': 55, 'INPLACE_LSHIFT': 75, 'INPLACE_MODULO': 59, 'STORE_ATTR': 95, 'BUILD_MAP': 104, 'BINARY_DIVIDE': 21, 'INPLACE_RSHIFT': 76, 'PRINT_ITEM_TO': 73, 'UNPACK_SEQUENCE': 92, 'BINARY_MULTIPLY': 20, 'MAKE_FUNCTION': 132, 'PRINT_NEWLINE_TO': 74, 'NOP': 9, 'LIST_APPEND': 18, 'INPLACE_XOR': 78, 'STORE_GLOBAL': 97, 'INPLACE_SUBTRACT': 56, 'INPLACE_POWER': 67, 'ROT_FOUR': 5, 'DELETE_SUBSCR': 61, 'BINARY_AND': 64, 'BREAK_LOOP': 80, 'JUMP_IF_FALSE': 111, 'DELETE_SLICE+1': 51, 'DELETE_SLICE+0': 50, 'DUP_TOPX': 99, 'CALL_FUNCTION_VAR_KW': 142, 'LOAD_ATTR': 105, 'BINARY_TRUE_DIVIDE': 27, 'ROT_TWO': 2, 'IMPORT_FROM': 108, 'DELETE_FAST': 126, 'BINARY_ADD': 23, 'LOAD_CONST': 100, 'STORE_DEREF': 137, 'UNARY_NEGATIVE': 11, 'UNARY_POSITIVE': 10, 'STORE_SUBSCR': 60, 'BUILD_TUPLE': 102, 'BINARY_POWER': 19, 'BUILD_CLASS': 89, 'UNARY_CONVERT': 13, 'BINARY_MODULO': 22, 'DELETE_SLICE+3': 53, 'DELETE_SLICE+2': 52, 'WITH_CLEANUP': 81, 'DELETE_ATTR': 96, 'PRINT_ITEM': 71, 'RAISE_VARARGS': 130, 'SLICE+0': 30, 'SLICE+1': 31, 'SLICE+2': 32, 'SLICE+3': 33, 'LOAD_DEREF': 136, 'LOAD_FAST': 124, 'BINARY_FLOOR_DIVIDE': 26, 'BINARY_RSHIFT': 63, 'BINARY_SUBSCR': 25, 'YIELD_VALUE': 86, 'ROT_THREE': 3, 'STORE_SLICE+0': 40, 'STORE_SLICE+1': 41, 'STORE_SLICE+2': 42, 'STORE_SLICE+3': 43, 'UNARY_INVERT': 15, 'PRINT_NEWLINE': 72, 'INPLACE_DIVIDE': 58, 'BUILD_SLICE': 133, 'JUMP_ABSOLUTE': 113, 'MAKE_CLOSURE': 134, 'JUMP_FORWARD': 110},
-    27: {'CALL_FUNCTION': 131, 'DUP_TOP': 4, 'INPLACE_FLOOR_DIVIDE': 28, 'MAP_ADD': 147, 'BINARY_XOR': 65, 'END_FINALLY': 88, 'RETURN_VALUE': 83, 'POP_BLOCK': 87, 'SETUP_LOOP': 120, 'BUILD_SET': 104, 'POP_TOP': 1, 'EXTENDED_ARG': 145, 'SETUP_FINALLY': 122, 'INPLACE_TRUE_DIVIDE': 29, 'CALL_FUNCTION_KW': 141, 'INPLACE_AND': 77, 'SETUP_EXCEPT': 121, 'STORE_NAME': 90, 'IMPORT_NAME': 108, 'LOAD_GLOBAL': 116, 'LOAD_NAME': 101, 'FOR_ITER': 93, 'EXEC_STMT': 85, 'DELETE_NAME': 91, 'BUILD_LIST': 103, 'COMPARE_OP': 107, 'BINARY_OR': 66, 'INPLACE_MULTIPLY': 57, 'STORE_FAST': 125, 'CALL_FUNCTION_VAR': 140, 'SET_ADD': 146, 'LOAD_LOCALS': 82, 'CONTINUE_LOOP': 119, 'PRINT_EXPR': 70, 'DELETE_GLOBAL': 98, 'GET_ITER': 68, 'STOP_CODE': 0, 'UNARY_NOT': 12, 'BINARY_LSHIFT': 62, 'LOAD_CLOSURE': 135, 'IMPORT_STAR': 84, 'INPLACE_OR': 79, 'BINARY_SUBTRACT': 24, 'STORE_MAP': 54, 'INPLACE_ADD': 55, 'INPLACE_LSHIFT': 75, 'INPLACE_MODULO': 59, 'STORE_ATTR': 95, 'BUILD_MAP': 105, 'SETUP_WITH': 143, 'BINARY_DIVIDE': 21, 'INPLACE_RSHIFT': 76, 'PRINT_ITEM_TO': 73, 'UNPACK_SEQUENCE': 92, 'BINARY_MULTIPLY': 20, 'PRINT_NEWLINE_TO': 74, 'NOP': 9, 'LIST_APPEND': 94, 'INPLACE_XOR': 78, 'STORE_GLOBAL': 97, 'INPLACE_SUBTRACT': 56, 'INPLACE_POWER': 67, 'ROT_FOUR': 5, 'DELETE_SUBSCR': 61, 'BINARY_AND': 64, 'BREAK_LOOP': 80, 'MAKE_FUNCTION': 132, 'DELETE_SLICE+1': 51, 'DELETE_SLICE+0': 50, 'DUP_TOPX': 99, 'CALL_FUNCTION_VAR_KW': 142, 'LOAD_ATTR': 106, 'BINARY_TRUE_DIVIDE': 27, 'ROT_TWO': 2, 'IMPORT_FROM': 109, 'DELETE_FAST': 126, 'BINARY_ADD': 23, 'LOAD_CONST': 100, 'STORE_DEREF': 137, 'UNARY_NEGATIVE': 11, 'UNARY_POSITIVE': 10, 'STORE_SUBSCR': 60, 'BUILD_TUPLE': 102, 'BINARY_POWER': 19, 'BUILD_CLASS': 89, 'UNARY_CONVERT': 13, 'BINARY_MODULO': 22, 'DELETE_SLICE+3': 53, 'DELETE_SLICE+2': 52, 'WITH_CLEANUP': 81, 'DELETE_ATTR': 96, 'POP_JUMP_IF_TRUE': 115, 'JUMP_IF_FALSE_OR_POP': 111, 'PRINT_ITEM': 71, 'RAISE_VARARGS': 130, 'SLICE+0': 30, 'SLICE+1': 31, 'SLICE+2': 32, 'SLICE+3': 33, 'POP_JUMP_IF_FALSE': 114, 'LOAD_DEREF': 136, 'LOAD_FAST': 124, 'JUMP_IF_TRUE_OR_POP': 112, 'BINARY_FLOOR_DIVIDE': 26, 'BINARY_RSHIFT': 63, 'BINARY_SUBSCR': 25, 'YIELD_VALUE': 86, 'ROT_THREE': 3, 'STORE_SLICE+0': 40, 'STORE_SLICE+1': 41, 'STORE_SLICE+2': 42, 'STORE_SLICE+3': 43, 'UNARY_INVERT': 15, 'PRINT_NEWLINE': 72, 'INPLACE_DIVIDE': 58, 'BUILD_SLICE': 133, 'JUMP_ABSOLUTE': 113, 'MAKE_CLOSURE': 134, 'JUMP_FORWARD': 110},
-    30: {'LOAD_CLOSURE': 135, 'POP_BLOCK': 87, 'DELETE_ATTR': 96, 'INPLACE_POWER': 67, 'LOAD_BUILD_CLASS': 71, 'BINARY_SUBSCR': 25, 'INPLACE_FLOOR_DIVIDE': 28, 'WITH_CLEANUP_START': 81, 'POP_TOP': 1, 'CALL_FUNCTION_VAR': 140, 'DUP_TOP_TWO': 5, 'CONTINUE_LOOP': 119, 'IMPORT_STAR': 84, 'GET_AITER': 50, 'RETURN_VALUE': 83, 'GET_AWAITABLE': 73, 'JUMP_IF_TRUE_OR_POP': 112, 'UNARY_INVERT': 15, 'YIELD_VALUE': 86, 'END_FINALLY': 88, 'BREAK_LOOP': 80, 'IMPORT_NAME': 108, 'UNPACK_SEQUENCE': 92, 'DELETE_SUBSCR': 61, 'DELETE_DEREF': 138, 'WITH_CLEANUP_FINISH': 82, 'YIELD_FROM': 72, 'UNARY_POSITIVE': 10, 'SETUP_LOOP': 120, 'STORE_ATTR': 95, 'SETUP_FINALLY': 122, 'SETUP_WITH': 143, 'MAKE_FUNCTION': 132, 'DELETE_GLOBAL': 98, 'IMPORT_FROM': 109, 'INPLACE_OR': 79, 'SET_ADD': 146, 'NOP': 9, 'BINARY_FLOOR_DIVIDE': 26, 'STORE_SUBSCR': 60, 'DELETE_FAST': 126, 'POP_JUMP_IF_TRUE': 115, 'BINARY_ADD': 23, 'BUILD_TUPLE': 102, 'LOAD_CONST': 100, 'BUILD_SET_UNPACK': 153, 'BINARY_MATRIX_MULTIPLY': 16, 'INPLACE_SUBTRACT': 56, 'INPLACE_XOR': 78, 'CALL_FUNCTION_KW': 141, 'INPLACE_MULTIPLY': 57, 'JUMP_ABSOLUTE': 113, 'BINARY_SUBTRACT': 24, 'CALL_FUNCTION_VAR_KW': 142, 'DUP_TOP': 4, 'BINARY_AND': 64, 'LOAD_ATTR': 106, 'LOAD_FAST': 124, 'UNARY_NOT': 12, 'BINARY_LSHIFT': 62, 'BINARY_XOR': 65, 'INPLACE_AND': 77, 'MAP_ADD': 147, 'BUILD_TUPLE_UNPACK': 152, 'EXTENDED_ARG': 144, 'BUILD_SET': 104, 'LIST_APPEND': 145, 'INPLACE_MODULO': 59, 'STORE_NAME': 90, 'JUMP_FORWARD': 110, 'BUILD_MAP_UNPACK': 150, 'COMPARE_OP': 107, 'LOAD_DEREF': 136, 'BINARY_RSHIFT': 63, 'LOAD_NAME': 101, 'BUILD_SLICE': 133, 'SETUP_ASYNC_WITH': 154, 'STORE_FAST': 125, 'INPLACE_RSHIFT': 76, 'ROT_TWO': 2, 'STORE_DEREF': 137, 'STORE_GLOBAL': 97, 'INPLACE_ADD': 55, 'FOR_ITER': 93, 'LOAD_CLASSDEREF': 148, 'BUILD_MAP_UNPACK_WITH_CALL': 151, 'INPLACE_LSHIFT': 75, 'BINARY_OR': 66, 'PRINT_EXPR': 70, 'BEFORE_ASYNC_WITH': 52, 'INPLACE_MATRIX_MULTIPLY': 17, 'BINARY_MODULO': 22, 'JUMP_IF_FALSE_OR_POP': 111, 'BINARY_TRUE_DIVIDE': 27, 'UNARY_NEGATIVE': 11, 'RAISE_VARARGS': 130, 'BUILD_MAP': 105, 'UNPACK_EX': 94, 'POP_JUMP_IF_FALSE': 114, 'BINARY_POWER': 19, 'BUILD_LIST_UNPACK': 149, 'INPLACE_TRUE_DIVIDE': 29, 'GET_YIELD_FROM_ITER': 69, 'GET_ANEXT': 51, 'POP_EXCEPT': 89, 'ROT_THREE': 3, 'BINARY_MULTIPLY': 20, 'GET_ITER': 68, 'BUILD_LIST': 103, 'CALL_FUNCTION': 131, 'DELETE_NAME': 91, 'MAKE_CLOSURE': 134, 'LOAD_GLOBAL': 116, 'SETUP_EXCEPT': 121},
-    None: opcode.opmap,
-}
-
-
-def translate_opcodes(src_code, dst_opmap):
+def translate_opcodes(src_code, dst_op_specs):
     """
-    Very crude inter-python version opcode translator. Raises key error when
+    Very crude inter-python version opcode translator. Raises SyntaxError when
     the opcode doesn't exist in the destination opmap. Used to transcribe
     python code objects between python versions.
 
@@ -112,22 +104,55 @@ def translate_opcodes(src_code, dst_opmap):
         dst_opmap(dict): The opcode mapping for the target.
 
     Returns:
-        bytes: The translated opcodes.
+        (bytes, int): The translated opcodes and the new stack size.
     """
 
-    dst_code = b''
+    dst_opmap = dst_op_specs['opmap']
 
-    code_iter = six.iterbytes(src_code)
-    for src_op_code in code_iter:
-        src_op_name = opcode.opname[src_op_code]
+    src_ops = pwnypack.bytecode.disassemble(src_code)
+    dst_ops = []
 
-        dst_code += six.int2byte(dst_opmap[src_op_name])
+    op_iter = enumerate(src_ops)
+    for i, op in op_iter:
+        if isinstance(op, pwnypack.bytecode.Label):
+            dst_ops.append(op)
+            continue
 
-        if src_op_code >= opcode.HAVE_ARGUMENT:
-            dst_code += six.int2byte(next(code_iter))
-            dst_code += six.int2byte(next(code_iter))
+        if op.name not in dst_opmap:
+            if op.name == 'POP_JUMP_IF_FALSE' and 'JUMP_IF_TRUE' in dst_opmap:
+                lbl = pwnypack.bytecode.Label()
+                dst_ops.extend([
+                    pwnypack.bytecode.Op('JUMP_IF_TRUE', lbl),
+                    pwnypack.bytecode.Op('POP_TOP', None),
+                    pwnypack.bytecode.Op('JUMP_ABSOLUTE', op.arg),
+                    lbl,
+                    pwnypack.bytecode.Op('POP_TOP', None),
+                ])
+            elif op.name == 'POP_JUMP_IF_TRUE' and 'JUMP_IF_FALSE' in dst_opmap:
+                lbl = pwnypack.bytecode.Label()
+                dst_ops.extend([
+                    pwnypack.bytecode.Op('JUMP_IF_FALSE', lbl),
+                    pwnypack.bytecode.Op('POP_TOP', None),
+                    pwnypack.bytecode.Op('JUMP_ABSOLUTE', op.arg),
+                    lbl,
+                    pwnypack.bytecode.Op('POP_TOP', None),
+                ])
+            elif op.name == 'JUMP_IF_FALSE' and 'JUMP_IF_FALSE_OR_POP' in dst_opmap and \
+                    src_ops[i + 1].name == 'POP_TOP':
+                next(op_iter)
+                dst_ops.append(pwnypack.bytecode.Op('JUMP_IF_FALSE_OR_POP', op.arg))
+            elif op.name == 'JUMP_IF_TRUE' and 'JUMP_IF_TRUE_OR_POP' in dst_opmap and \
+                    src_ops[i + 1].name == 'POP_TOP':
+                next(op_iter)
+                dst_ops.append(pwnypack.bytecode.Op('JUMP_IF_TRUE_OR_POP', op.arg))
+            else:
+                raise SyntaxError('Opcode %s not supported on target.' % op.name)
+        else:
+            dst_ops.append(op)
 
-    return dst_code
+    dst_bytecode = pwnypack.bytecode.assemble(dst_ops, dst_op_specs)
+    dst_stacksize = pwnypack.bytecode.calculate_max_stack_depth(dst_ops, dst_op_specs)
+    return dst_bytecode, dst_stacksize
 
 
 @kwonly_defaults
@@ -143,6 +168,19 @@ def pickle_func(func, target=None, protocol=None, b64encode=None, *args):
         transcribed to try to maintain compatibility. If an opcode is emitted
         which is not supported by the target python version, a KeyError will
         be raised.
+
+        Constructs that are known to be problematic:
+
+        - Python 2.6 and 2.7/3.0 use very different, incompatible opcodes for
+          conditional jumps (if, while, etc). Serializing those is not
+          always possible between python 2.6 to 2.7/3.0.
+
+        - Exception handling uses different, incompatible opcodes between
+          python 2 and 3.
+
+        - Python 2 and python 3 handle nested functions very differently: the
+          same opcode is used in a different way and leads to a crash. Avoid
+          nesting functions if you want to pickle across python functions.
 
     Arguments:
         func(callable): The function to serialize and call when unpickled.
@@ -173,7 +211,7 @@ def pickle_func(func, target=None, protocol=None, b64encode=None, *args):
 
     def code_reduce_v2(code):
         # Translate the opcodes to the target python's opcode map.
-        co_code = translate_opcodes(code.co_code, OPMAPS[target])
+        co_code, co_stacksize = translate_opcodes(code.co_code, pwnypack.bytecode.OP_SPECS[target])
 
         if b64encode:
             # b64encode co_code and co_lnotab as they contain 8bit data.
@@ -194,13 +232,13 @@ def pickle_func(func, target=None, protocol=None, b64encode=None, *args):
             co_filename = code.co_filename
             co_name = code.co_name
 
-        return types.CodeType, (code.co_argcount, code.co_nlocals, code.co_stacksize, code.co_flags,
+        return types.CodeType, (code.co_argcount, code.co_nlocals, co_stacksize, code.co_flags,
                                 co_code, code.co_consts, co_names, co_varnames, co_filename,
                                 co_name, code.co_firstlineno, co_lnotab)
 
     def code_reduce_v3(code):
         # Translate the opcodes to the target python's opcode map.
-        co_code = translate_opcodes(code.co_code, OPMAPS[target])
+        co_code, co_stacksize = translate_opcodes(code.co_code, pwnypack.bytecode.OP_SPECS[target])
 
         if b64encode:
             # b64encode co_code and co_lnotab as they contain 8bit data.
@@ -214,7 +252,7 @@ def pickle_func(func, target=None, protocol=None, b64encode=None, *args):
         else:
             co_kwonlyargcount = code.co_kwonlyargcount
 
-        return types.CodeType, (code.co_argcount, co_kwonlyargcount, code.co_nlocals, code.co_stacksize,
+        return types.CodeType, (code.co_argcount, co_kwonlyargcount, code.co_nlocals, co_stacksize,
                                 code.co_flags, co_code, code.co_consts, code.co_names, code.co_varnames,
                                 code.co_filename, code.co_name, code.co_firstlineno, co_lnotab)
 
