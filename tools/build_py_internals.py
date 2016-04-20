a240f5de185a8336f687655c84d4b897fb6ee73e
Ingmar Steen <iksteen@gmail.com>
2016-04-20 09:47:24 +0200

    All PY_INTERNALS get their own global.

--- tools/build_py_internals.py
+++ tools/build_py_internals.py
@@ -13,10 +13,10 @@ import os
 import sys
 import subprocess
 
-
-# These are pretty much hand-crafted dicts from various python version's Python/compile.c.
 import six
 
+
+# These are pretty much hand-crafted dicts from various python version's Python/compile.c.
 STACK_EFFECT = {
     26: {'EXTENDED_ARG': None, 'STOP_CODE': None, 'NOP': 0, 'POP_TOP': -1, 'ROT_TWO': 0, 'ROT_THREE': 0, 'DUP_TOP': 1, 'ROT_FOUR': 0, 'UNARY_POSITIVE': 0, 'UNARY_NEGATIVE': 0, 'UNARY_NOT': 0, 'UNARY_CONVERT': 0, 'UNARY_INVERT': 0, 'LIST_APPEND': -2, 'BINARY_POWER': -1, 'BINARY_MULTIPLY': -1, 'BINARY_DIVIDE': -1, 'BINARY_MODULO': -1, 'BINARY_ADD': -1, 'BINARY_SUBTRACT': -1, 'BINARY_SUBSCR': -1, 'BINARY_FLOOR_DIVIDE': -1, 'BINARY_TRUE_DIVIDE': -1, 'INPLACE_FLOOR_DIVIDE': -1, 'INPLACE_TRUE_DIVIDE': -1, 'SLICE+0': 1, 'SLICE+1': 0, 'SLICE+2': 0, 'SLICE+3': -1, 'STORE_SLICE+0': -2, 'STORE_SLICE+1': -3, 'STORE_SLICE+2': -3, 'STORE_SLICE+3': -4, 'DELETE_SLICE+0': -1, 'DELETE_SLICE+1': -2, 'DELETE_SLICE+2': -2, 'DELETE_SLICE+3': -3, 'INPLACE_ADD': -1, 'INPLACE_SUBTRACT': -1, 'INPLACE_MULTIPLY': -1, 'INPLACE_DIVIDE': -1, 'INPLACE_MODULO': -1, 'STORE_SUBSCR': -3, 'STORE_MAP': -2, 'DELETE_SUBSCR': -2, 'BINARY_LSHIFT': -1, 'BINARY_RSHIFT': -1, 'BINARY_AND': -1, 'BINARY_XOR': -1, 'BINARY_OR': -1, 'INPLACE_POWER': -1, 'GET_ITER': 0, 'PRINT_EXPR': -1, 'PRINT_ITEM': -1, 'PRINT_NEWLINE': 0, 'PRINT_ITEM_TO': -2, 'PRINT_NEWLINE_TO': -1, 'INPLACE_LSHIFT': -1, 'INPLACE_RSHIFT': -1, 'INPLACE_AND': -1, 'INPLACE_XOR': -1, 'INPLACE_OR': -1, 'BREAK_LOOP': 0, 'WITH_CLEANUP': -1, 'LOAD_LOCALS': 1, 'RETURN_VALUE': -1, 'IMPORT_STAR': -1, 'EXEC_STMT': -3, 'YIELD_VALUE': 0, 'POP_BLOCK': 0, 'END_FINALLY': -1, 'BUILD_CLASS': -2, 'STORE_NAME': -1, 'DELETE_NAME': 0, 'UNPACK_SEQUENCE': 'lambda arg: arg - 1', 'FOR_ITER': 1, 'STORE_ATTR': -2, 'DELETE_ATTR': -1, 'STORE_GLOBAL': -1, 'DELETE_GLOBAL': 0, 'DUP_TOPX': 'lambda arg: arg', 'LOAD_CONST': 1, 'LOAD_NAME': 1, 'BUILD_TUPLE': 'lambda arg: 1 - arg', 'BUILD_LIST': 'lambda arg: 1 - arg', 'BUILD_MAP': 1, 'LOAD_ATTR': 0, 'COMPARE_OP': -1, 'IMPORT_NAME': 0, 'IMPORT_FROM': 1, 'JUMP_FORWARD': 0, 'JUMP_IF_FALSE': 0, 'JUMP_IF_TRUE': 0, 'JUMP_ABSOLUTE': 0, 'LOAD_GLOBAL': 1, 'CONTINUE_LOOP': 0, 'SETUP_LOOP': 0, 'SETUP_EXCEPT': 3, 'SETUP_FINALLY': 3, 'LOAD_FAST': 1, 'STORE_FAST': -1, 'DELETE_FAST': 0, 'RAISE_VARARGS': 'lambda arg: -arg', 'CALL_FUNCTION': 'lambda arg: -((arg % 256) + 2 * (arg >> 8))', 'CALL_FUNCTION_VAR': 'lambda arg: -((arg % 256) + 2 * (arg >> 8)) - 1', 'CALL_FUNCTION_KW': 'lambda arg: -((arg % 256) + 2 * (arg >> 8)) - 1', 'CALL_FUNCTION_VAR_KW': 'lambda arg: -((arg % 256) + 2 * (arg >> 8)) - 2', 'MAKE_FUNCTION': 'lambda arg: -arg', 'BUILD_SLICE': 'lambda arg: -2 if arg == 3 else -1', 'MAKE_CLOSURE': 'lambda arg: -arg', 'LOAD_CLOSURE': 1, 'LOAD_DEREF': 1, 'STORE_DEREF': -1},
     27: {'EXTENDED_ARG': None, 'STOP_CODE': None, 'NOP': 0, 'POP_TOP': -1, 'ROT_TWO': 0, 'ROT_THREE': 0, 'DUP_TOP': 1, 'ROT_FOUR': 0, 'UNARY_POSITIVE': 0, 'UNARY_NEGATIVE': 0, 'UNARY_NOT': 0, 'UNARY_CONVERT': 0, 'UNARY_INVERT': 0, 'SET_ADD': -1, 'LIST_APPEND': -1, 'MAP_ADD': -2, 'BINARY_POWER': -1, 'BINARY_MULTIPLY': -1, 'BINARY_DIVIDE': -1, 'BINARY_MODULO': -1, 'BINARY_ADD': -1, 'BINARY_SUBTRACT': -1, 'BINARY_SUBSCR': -1, 'BINARY_FLOOR_DIVIDE': -1, 'BINARY_TRUE_DIVIDE': -1, 'INPLACE_FLOOR_DIVIDE': -1, 'INPLACE_TRUE_DIVIDE': -1, 'SLICE+0': 0, 'SLICE+1': -1, 'SLICE+2': -1, 'SLICE+3': -2, 'STORE_SLICE+0': -2, 'STORE_SLICE+1': -3, 'STORE_SLICE+2': -3, 'STORE_SLICE+3': -4, 'DELETE_SLICE+0': -1, 'DELETE_SLICE+1': -2, 'DELETE_SLICE+2': -2, 'DELETE_SLICE+3': -3, 'INPLACE_ADD': -1, 'INPLACE_SUBTRACT': -1, 'INPLACE_MULTIPLY': -1, 'INPLACE_DIVIDE': -1, 'INPLACE_MODULO': -1, 'STORE_SUBSCR': -3, 'STORE_MAP': -2, 'DELETE_SUBSCR': -2, 'BINARY_LSHIFT': -1, 'BINARY_RSHIFT': -1, 'BINARY_AND': -1, 'BINARY_XOR': -1, 'BINARY_OR': -1, 'INPLACE_POWER': -1, 'GET_ITER': 0, 'PRINT_EXPR': -1, 'PRINT_ITEM': -1, 'PRINT_NEWLINE': 0, 'PRINT_ITEM_TO': -2, 'PRINT_NEWLINE_TO': -1, 'INPLACE_LSHIFT': -1, 'INPLACE_RSHIFT': -1, 'INPLACE_AND': -1, 'INPLACE_XOR': -1, 'INPLACE_OR': -1, 'BREAK_LOOP': 0, 'SETUP_WITH': 4, 'WITH_CLEANUP': -1, 'LOAD_LOCALS': 1, 'RETURN_VALUE': -1, 'IMPORT_STAR': -1, 'EXEC_STMT': -3, 'YIELD_VALUE': 0, 'POP_BLOCK': 0, 'END_FINALLY': -3, 'BUILD_CLASS': -2, 'STORE_NAME': -1, 'DELETE_NAME': 0, 'UNPACK_SEQUENCE': 'lambda arg: arg - 1', 'FOR_ITER': 1, 'STORE_ATTR': -2, 'DELETE_ATTR': -1, 'STORE_GLOBAL': -1, 'DELETE_GLOBAL': 0, 'DUP_TOPX': 'lambda arg: arg', 'LOAD_CONST': 1, 'LOAD_NAME': 1, 'BUILD_TUPLE': 'lambda arg: 1 - arg', 'BUILD_LIST': 'lambda arg: 1 - arg', 'BUILD_SET': 'lambda arg: 1 - arg', 'BUILD_MAP': 1, 'LOAD_ATTR': 0, 'COMPARE_OP': -1, 'IMPORT_NAME': -1, 'IMPORT_FROM': 1, 'JUMP_FORWARD': 0, 'JUMP_IF_FALSE_OR_POP': 0, 'JUMP_IF_TRUE_OR_POP': 0, 'JUMP_ABSOLUTE': 0, 'POP_JUMP_IF_FALSE': -1, 'POP_JUMP_IF_TRUE': -1, 'LOAD_GLOBAL': 1, 'CONTINUE_LOOP': 0, 'SETUP_LOOP': 0, 'SETUP_EXCEPT': 0, 'SETUP_FINALLY': 0, 'LOAD_FAST': 1, 'STORE_FAST': -1, 'DELETE_FAST': 0, 'RAISE_VARARGS': 'lambda arg: -arg', 'CALL_FUNCTION': 'lambda arg: -((arg % 256) + 2 * (arg >> 8))', 'CALL_FUNCTION_VAR': 'lambda arg: -((arg % 256) + 2 * (arg >> 8)) - 1', 'CALL_FUNCTION_KW': 'lambda arg: -((arg % 256) + 2 * (arg >> 8)) - 1', 'CALL_FUNCTION_VAR_KW': 'lambda arg: -((arg % 256) + 2 * (arg >> 8)) - 2', 'MAKE_FUNCTION': 'lambda arg: -arg', 'BUILD_SLICE': 'lambda arg: -2 if arg == 3 else -1', 'MAKE_CLOSURE': 'lambda arg: -arg - 1', 'LOAD_CLOSURE': 1, 'LOAD_DEREF': 1, 'STORE_DEREF': -1},
@@ -29,6 +29,9 @@ STACK_EFFECT = {
 }
 
 
+# 0: No specifics
+# 1: Target depth adjustment for FOR_ITER, SETUP_FINALLY and SETUP_EXCEPT
+# 2: Target depth adjustment for JUMP_IF_TRUE_OR_POP and JUMP_IF_FALSE_OR_POP
 STACK_TRAITS = {
     26: 0,
     27: 3,
@@ -114,6 +117,7 @@ def collect_version_info():
 
 if __name__ == '__main__':
     version_info = collect_version_info()
+    versions = sorted(six.iteritems(version_info), key=lambda i: i[0])
 
     print('''# DO NOT EDIT THIS FILE!
 # This file is automatically generated by build_py_internals.py.
@@ -128,39 +132,57 @@ the ``build_py_internals.py`` script.
 
 
 import sys
+import warnings
 
 
-__all__ = ['PY_INTERNALS']
+__all__ = ['PY_INTERNALS', {}]
 
+'''.format(', '.join([repr('PY_{}'.format(version)) for version, _ in versions])))
 
-#: This dictionary describes the internals of various python versions.
-PY_INTERNALS = {''')
-    for version in sorted(version_info):
-        info = version_info[version]
-        print('''    {version}: {{
-        'version': {version},
-        'magic': {info[magic]},
-        'pickle_highest_protocol': {info[pickle_highest_protocol]},
-        'pickle_default_protocol': {info[pickle_default_protocol]},
-        'extended_arg': {info[extended_arg]},
-        'have_argument': {info[have_argument]},
-        'hascompare': {info[hascompare]!r},
-        'hasconst': {info[hasconst]!r},
-        'hasfree': {info[hasfree]!r},
-        'haslocal': {info[haslocal]!r},
-        'hasname': {info[hasname]!r},
-        'hasjrel': {info[hasjrel]!r},
-        'hasjabs': {info[hasjabs]!r},
-        'cmp_op': {info[cmp_op]},
-        'opmap': {info[opmap]},
-        'opname': {info[opname]},
-        'stackeffect': {stackeffect},
-        'stackeffect_traits': {stackeffect_traits},
-    }},'''.format(
+    for version, info in versions:
+        stackeffect = '{{{}}}'.format(
+            ', '.join(
+                '{0!r}: {1}'.format(k, v)
+                for k, v in six.iteritems(info['stackeffect'])
+            )
+        )
+        print('''#: This dictionary describes the internals of CPython {major}.{minor}.
+PY_{version} = {{
+    'version': {version},
+    'magic': {info[magic]},
+    'pickle_highest_protocol': {info[pickle_highest_protocol]},
+    'pickle_default_protocol': {info[pickle_default_protocol]},
+    'extended_arg': {info[extended_arg]},
+    'have_argument': {info[have_argument]},
+    'hascompare': {info[hascompare]!r},
+    'hasconst': {info[hasconst]!r},
+    'hasfree': {info[hasfree]!r},
+    'haslocal': {info[haslocal]!r},
+    'hasname': {info[hasname]!r},
+    'hasjrel': {info[hasjrel]!r},
+    'hasjabs': {info[hasjabs]!r},
+    'cmp_op': {info[cmp_op]},
+    'opmap': {info[opmap]},
+    'opname': {info[opname]},
+    'stackeffect': {stackeffect},
+    'stackeffect_traits': {stackeffect_traits},
+}}
+'''.format(
             version=version,
+            major=version // 10,
+            minor=version % 10,
             info=info,
-            stackeffect='{{{}}}'.format(', '.join('{0!r}: {1}'.format(k, v) for k, v in six.iteritems(info['stackeffect']))),
+            stackeffect=stackeffect,
             stackeffect_traits=STACK_TRAITS[version]
         ))
+
+    print('''
+#: This dictionary describes the internals of various python versions.
+PY_INTERNALS = {''')
+    for version, _ in versions:
+        print('    {0}: PY_{0},'.format(version))
     print('}')
-    print('PY_INTERNALS[None] = PY_INTERNALS[sys.version_info[0] * 10 + sys.version_info[1]]')
+
+    print('''PY_INTERNALS[None] = PY_INTERNALS.get(sys.version_info[0] * 10 + sys.version_info[1])
+if PY_INTERNALS[None] is None:
+    warnings.warn('The running python version is unsupported, explicit internals will be required.')''')
