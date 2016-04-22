f97b12667196a25e58345a494f65405311ef701b
Ingmar Steen <iksteen@gmail.com>
2016-04-22 16:29:19 +0200

    Add test for shellcode generation.
    The output isn’t actually tested, but it hits quite a few code-paths.

---
+++ tests/test_shellcode.py
@@ -0,0 +1,77 @@
+import pytest
+import sys
+
+from pwny import *
+
+
+@pytest.mark.xfail(sys.version_info < (2, 7),
+                   reason="inspect.getcallargs new in python 2.7")
+def test_shellcode_translate():
+    @sc.LinuxX86.translate()
+    def shellcode():
+        buf = alloc_buffer(64)
+        sys_read(0, buf, buf.length)
+        sys_write(1, buf, buf.length)
+        sys_setresuid(sys_geteuid(), sys_geteuid(), sys_geteuid())
+        sys_execve(u'/bin/sh', [u'/bin/sh', u'-c', buf, None], None)
+    shellcode()
+
+
+SHELLCODE_ENVS = [
+    (sc.LinuxX86, ()),
+    (sc.LinuxX86NullSafeMutable, ()),
+    (sc.LinuxX86_64, ()),
+    (sc.LinuxX86_64NullSafeMutable, ()),
+
+    (sc.LinuxARM, ()),
+    (sc.LinuxARM, (Target.Endian.little,)),
+    (sc.LinuxARM, (Target.Endian.big,)),
+
+    (sc.LinuxARMThumb, ()),
+    (sc.LinuxARMThumb, (Target.Endian.little,)),
+    (sc.LinuxARMThumb, (Target.Endian.big,)),
+
+    (sc.LinuxARMThumbMixed, ()),
+    (sc.LinuxARMThumbMixed, (Target.Endian.little,)),
+    (sc.LinuxARMThumbMixed, (Target.Endian.big,)),
+
+    (sc.LinuxAArch64, ()),
+    (sc.LinuxAArch64, (Target.Endian.little,)),
+    (sc.LinuxAArch64, (Target.Endian.big,)),
+]
+
+
+@pytest.mark.parametrize(('env_type', 'env_args'), SHELLCODE_ENVS)
+def test_shellcode_env_simple(env_type, env_args):
+    env = env_type(*env_args)
+    env.compile([
+        env.sys_exit(0),
+    ])
+
+
+@pytest.mark.parametrize(('env_type', 'env_args'), SHELLCODE_ENVS)
+def test_shellcode_env_complex(env_type, env_args):
+    env = env_type(*env_args)
+    buf = env.alloc_buffer(64)
+    env.compile([
+        sc.LoadRegister(env.SYSCALL_REG, 0xdeadcafe),
+        env.sys_read(0, buf, buf.length - 1),
+        env.sys_write(1, buf, buf.length - 1),
+        env.sys_setresuid(env.sys_geteuid(), env.sys_geteuid(), env.sys_geteuid()),
+        env.sys_execve(u'/bin/sh', [u'/bin/sh', u'-c', buf, None], None),
+    ])
+
+
+@pytest.mark.parametrize(('env_type', 'env_args'), SHELLCODE_ENVS)
+@pytest.mark.xfail(raises=RuntimeError,
+                   reason='proper binutils missing on CI system')
+def test_shellcode_env_complex(env_type, env_args):
+    env = env_type(*env_args)
+    buf = env.alloc_buffer(64)
+    env.compile([
+        sc.LoadRegister(env.SYSCALL_REG, 0xdeadcafe),
+        env.sys_read(0, buf, buf.length - 1),
+        env.sys_write(1, buf, buf.length - 1),
+        env.sys_setresuid(env.sys_geteuid(), env.sys_geteuid(), env.sys_geteuid()),
+        env.sys_execve(u'/bin/sh', [u'/bin/sh', u'-c', buf, None], None),
+    ])
